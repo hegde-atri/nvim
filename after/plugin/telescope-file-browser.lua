2060695fac9c1c0457711b019ce5b4efa5129fd3
Atri Hegde <iamatrihegde@outlook.com>
2023-11-07 22:06:41 +0000

    (restart): using kickstart nvim

--- after/plugin/telescope-file-browser.lua
+++
@@ -1,8 +0,0 @@
-require("telescope").load_extension "file_browser"
-
-vim.api.nvim_set_keymap(
-    "n",
-    "<space>op",
-    ":Telescope file_browser",
-    { noremap = true }
-)
--- lua/core/init.lua
+++
@@ -1,5 +0,0 @@
-require("core.base")
-require("core.remap")
-require("core.packer")
-
-
